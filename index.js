f588ef3022a6aa4911d5e3dbc40a61d082f551d1
AVIJIT BISWAS <112837308+AVIJIT10-ops@users.noreply.github.com>
2023-04-20 21:54:10 +0600

    drogon-server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const categories = require('./Data/categories.json')
+app.use(cors())
+
+ app.get('/', (req, res) => {
+  res.send('Dragon is sruning')
+});
+app.get('/categories', (req,res) =>{
+    res.send(categories)
+})
+app.listen(port, () => {
+    console.log(`Dragon Api is running on port: ${port}`)
+})
